426895442d11c4ce7aa42fb45e011214afb3a03d
Seybel <seyibello31@gmail.com>
2020-05-13 01:11:07 +0100

    [Feat]updated charts dynamically

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="md-layout">
+    <div v-if="options.xaxis.categories.length" class="md-layout">
+        <!-- {{ testing.confirmed }} -->
         <!-- <div class="md-layout-row"> -->
             <div class="md-layout-item">
                 <div class="chart-inner-wrapper">
-                    <apexchart width="394" height="300" type="bar" :options="options" :series="seriesA"></apexchart>
+                  <!-- <button type="button" @click="updateC">Change data</button> -->
+                  <apexchart width="394" height="300" type="area" :options="options" :series="seriesA"></apexchart>
                 </div>
             </div>
             <div class="md-layout-item">
@@ -13,12 +15,12 @@
             </div>
              <div class="md-layout-item">
                 <div class="chart-inner-wrapper3">
-                    <apexchart width="394" height="300" type="line" :options="options" :series="seriesB"></apexchart>
+                    <apexchart width="394" height="300" type="line" :options="options" :series="seriesC"></apexchart>
                 </div>
             </div>
             <div class="md-layout-item">
                 <div class="chart-inner-wrapper4">
-                    <apexchart width="394" height="300" type="bar" :options="options" :series="seriesA"></apexchart>
+                    <apexchart width="394" height="300" type="bar" :options="options" :series="seriesD"></apexchart>
                 </div>
             </div>
         </div>
@@ -26,10 +28,13 @@
 
 <script>
 import axios from 'axios'
+// import VueApexCharts from 'vue-apexcharts'
 
 export default {
   data: () => ({
     testing: [],
+    testing2: [],
+    testing3: [],
     options: {
       chart: {
         id: 'vuechart-example',
@@ -37,8 +42,14 @@ export default {
           show: false
         }
       },
+      dataLabels: {
+        enabled: false
+      },
+      tooltip: {
+        theme: 'dark'
+      },
       xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        categories: [],
         labels: {
           show: true,
           style: {
@@ -55,7 +66,7 @@ export default {
         }
       },
       title: {
-        text: 'Vuechart-example',
+        text: 'My Charts',
         style: {
           fontSize: '16px',
           fontWeight: 'bold',
@@ -63,25 +74,104 @@ export default {
           color: '#7d7d7d'
         }
       },
+      noData: {
+        text: 'Loading...',
+        align: 'center',
+        verticalAlign: 'middle',
+        style: {
+          color: '#7d7d7d',
+          fontSize: '14px',
+          fontFamily: 'Helvetica, Arial, sans-serif'
+        }
+      },
       colors: ['#00897b']
     },
     seriesA: [{
-      name: 'series-1',
-      data: [30, 40, 45, 50, 49, 60, 70, 91]
+      name: 'Total Confirmed Cases',
+      data: []
     }],
     seriesB: [{
       name: 'series-2',
-      data: [30, 50, 65, 80, 99, 160, 170, 191]
+      data: [121, 141, 111, 131, 253, 161, 735]
+    }],
+    seriesC: [{
+      name: 'series-2',
+      data: [1211, 1141, 5051, 2331, 2653, 161, 335]
+    }],
+    seriesD: [{
+      name: 'series-2',
+      data: [121, 141, 111, 131, 253, 161, 735]
     }]
   }),
 
+  // mounted () {
+  //   this.updateC()
+  // },
+
+  //   // const newData = this.xAxis[0].data.map(() => {
+  //   //   return this.testing
+  //   // })
+  //   const newData = this.options.xaxis.categories.map(() => {
+  //     return this.testing
+  //   })
+  //   return newData
+  //   // In the same way, update the series option
+  //   // this.xAxis = [{
+  //   //   data: newData
+  //   // }]
+  // },
+
   mounted () {
-    axios.get('https://corona.lmao.ninja/v2/historical')
+    axios.get('https://covidapi.info/api/v1/global/count')
       .then(response => {
-        const characters = response.data
-        this.testing = characters
-        // display test results
-        // console.log(characters)
+        const myObj = response.data.result
+        const sliced = Object.keys(myObj).slice(-7).reduce((resultA, key) => {
+          resultA[key] = myObj[key]
+          return resultA
+        }, {})
+
+        // Separate dates from object
+        var allDates = sliced
+        var reportDates = Object.keys(allDates)
+        // console.log(reportDates)
+
+        // push report dates
+        this.options.xaxis.categories.push(...reportDates)
+
+        // Separate case, deaths, and recovered
+        var allCasesType = sliced
+        var CasesType = Object.values(allCasesType)
+
+        var t = CasesType.map((arr) => {
+          return arr.confirmed
+        })
+        console.log(t)
+        this.testing2 = CasesType.map((arr) => {
+          return arr.deaths
+        })
+        this.testing3 = CasesType.map((arr) => {
+          return arr.recovered
+        })
+        // console.log(CasesType)
+
+        // push series (confirmed)
+        this.seriesA[0].data.push(...t)
+
+        // console.log(sliced)
+
+        // this.testing = sliced
+        // return sliced
+
+        // const characters = response.data
+        // const myTest = characters.map(characters => characters.timeline.cases)
+        // var testResult = Object.keys(myTest).map(function (key) {
+        //   return [Number(key), myTest[key]]
+        // })
+        // console.log(testResult)
+        // const myTest2 = characters.map(characters => characters.timeline.deaths)
+
+        // this.testing2 = myTest2
+        // this.testing3 = myTest3
       }).catch(error => {
         console.log(`API: ${error}`)
       })
